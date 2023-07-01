1aebebd7aa8043d5fcd6a284102f4f2554f85a8a
naomicherono <nonniecheronoh12@gmail.com>
2023-07-01 23:27:21 +0300

    added property to populate the movie list

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-/ Fetch JSON data from localhost server
+// Fetch JSON data from localhost server
 fetch('http://localhost:3000/films')
   .then(response => response.json())
   .then(data => {
     const filmsList = document.getElementById('films');
+
+
+    // Populate movie list
+    data.forEach(movie => {
+        const li = document.createElement('li');
+        li.textContent = movie.title;
+        li.addEventListener('click', () => showMovieDetails(movie));
+        filmsList.appendChild(li);
+      });
